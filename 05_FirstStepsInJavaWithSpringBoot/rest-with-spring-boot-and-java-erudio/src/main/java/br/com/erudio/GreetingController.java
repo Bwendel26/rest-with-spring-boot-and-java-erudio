2f88b201da3a0a3bf4486269f2e8b7b09143c1cf
Bwendel26 <bwendel26999@gmail.com>
2022-12-17 14:15:00 -0300

    Adcionando arquivos iniciais para endpoint Greeting e gitignore file

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,22 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	public static final String template = "Hello, %s!";
+	public final AtomicLong counterId = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World")
+			String name) {	
+		return new Greeting(counterId.incrementAndGet(), template.format(template, name));
+	}
+			
+}
